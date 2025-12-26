b415fa3eb9dfc51d106111f33d15ac350dafc6f8
Ivan C Myrvold <ivan@myrvold.org>
2025-12-26 10:32:29 +0100

    removed SQLite dependence

--- Package.swift
+++ Package.swift
@@ -8,12 +8,10 @@ let package = Package(
   ],
   products: [
     .library(name: "CocoCashuCore", targets: ["CocoCashuCore"]),
-    .library(name: "CocoCashuSQLite", targets: ["CocoCashuSQLite"]),
     .library(name: "CocoCashuUI", targets: ["CocoCashuUI"]),
   ],
   dependencies: [
     .package(url: "https://github.com/GigaBitcoin/secp256k1.swift", exact: "0.2.0"),
-    .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.27.0")
   ],
   targets: [
     .target(
@@ -22,14 +20,7 @@ let package = Package(
         .product(name: "secp256k1", package: "secp256k1.swift")
       ]
     ),
-    .target(name: "CocoCashuSQLite", dependencies: [
-      "CocoCashuCore", .product(name: "GRDB", package: "GRDB.swift")
-    ]),
     .target(name: "CocoCashuUI", dependencies: ["CocoCashuCore"]),
     .testTarget(name: "CocoCashuCoreTests", dependencies: ["CocoCashuCore"]),
-//    .testTarget(
-//      name: "CocoCashuSQLiteTests",
-//      dependencies: ["CocoCashuSQLite"]
-//    )
   ]
 )
